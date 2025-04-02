c779803f18900b0faa4886f51c789f3f075e30d8
Ben Campbell <benc6053@gmail.com>
2025-04-02 19:13:49 -0400

    about to add about page

--- resources/js/pages/about.tsx
+++ resources/js/pages/about.tsx
@@ -6,7 +6,7 @@ export default function Welcome() {
 
     return (
         <>
-            <Head title="welcome Laravel Demo">
+            <Head title="About Laravel Demo">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
